a4908c4ac90d92d512286ba6cff708a0feaca6ce
Kushagra102 <kushagra.garg77@gmail.com>
2024-06-17 02:02:33 +0530

    feat: Kafka Logger
    Co-authored-by: Geoffrey Anto <Geoffrey-Anto@users.noreply.github.com>

---
+++ kafka-logger/go.mod
@@ -0,0 +1,14 @@
+module kafka-logger
+
+go 1.22.2
+
+require (
+	github.com/aws/aws-sdk-go v1.54.2
+	github.com/google/uuid v1.6.0
+	github.com/zhexuany/wordGenerator v0.0.0-20161102120352-1f13e790d534
+)
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ kafka-logger/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/google/uuid"
+)
+
+var (
+	cwl           *cloudwatchlogs.CloudWatchLogs
+	logGroupName  = "eventriest-logs"
+	logStreamName = ""
+	sequenceToken = ""
+)
+
+func init() {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String("ap-south-1"),
+			Credentials: credentials.NewEnvCredentials(),
+		},
+
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	cwl = cloudwatchlogs.New(sess)
+
+	err = ensureLogGroupExists(logGroupName)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func main() {
+	kafkaServer := os.Getenv("KAFKA_SERVER_URL")
+	kafkaTopic := os.Getenv("KAFKA_TOPIC")
+	groupID := os.Getenv("KAFKA_GROUP_ID")
+
+	config := kafka.ConfigMap{"bootstrap.servers": kafkaServer, "group.id": groupID, "go.events.channel.enable": true}
+
+	consumer, consumerCreateErr := kafka.NewConsumer(&config)
+
+	if consumerCreateErr != nil {
+		fmt.Println("consumer not created ", consumerCreateErr.Error())
+		os.Exit(1)
+	}
+
+	subscriptionErr := consumer.Subscribe(kafkaTopic, nil)
+
+	if subscriptionErr != nil {
+		fmt.Println("Unable to subscribe to topic " + kafkaTopic + " due to error - " + subscriptionErr.Error())
+		os.Exit(1)
+	} else {
+		fmt.Println("subscribed to topic ", kafkaTopic)
+	}
+
+	go processQueue(consumer)
+
+	// to stop the code from exiting
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
+}
+
+// ensureLogGroupExists first checks if the log group exists,
+// if it doesn't it will create one.
+func ensureLogGroupExists(name string) error {
+	resp, err := cwl.DescribeLogGroups(&cloudwatchlogs.DescribeLogGroupsInput{})
+	if err != nil {
+		return err
+	}
+
+	for _, logGroup := range resp.LogGroups {
+		if *logGroup.LogGroupName == name {
+			return nil
+		}
+	}
+
+	_, err = cwl.CreateLogGroup(&cloudwatchlogs.CreateLogGroupInput{
+		LogGroupName: &name,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = cwl.PutRetentionPolicy(&cloudwatchlogs.PutRetentionPolicyInput{
+		RetentionInDays: aws.Int64(14),
+		LogGroupName:    &name,
+	})
+
+	return err
+}
+
+// createLogStream will make a new logStream with a random uuid as its name.
+func createLogStream() error {
+	name := uuid.New().String()
+
+	_, err := cwl.CreateLogStream(&cloudwatchlogs.CreateLogStreamInput{
+		LogGroupName:  &logGroupName,
+		LogStreamName: &name,
+	})
+
+	logStreamName = name
+
+	return err
+}
+
+// processQueue will process the log queue
+func processQueue(consumer *kafka.Consumer) error {
+	var logQueue []*cloudwatchlogs.InputLogEvent
+
+	for {
+		fmt.Println("waiting for event...")
+		kafkaEvent := <-consumer.Events()
+		if kafkaEvent != nil {
+			switch event := kafkaEvent.(type) {
+			case *kafka.Message:
+				log := string(event.Value)
+				logQueue = append(logQueue, &cloudwatchlogs.InputLogEvent{
+					Message:   &log,
+					Timestamp: aws.Int64(time.Now().UnixNano() / int64(time.Millisecond)),
+				})
+			case kafka.Error:
+				fmt.Println("Consumer error ", event.String())
+			case kafka.PartitionEOF:
+				fmt.Println(kafkaEvent)
+			default:
+				fmt.Println(kafkaEvent)
+			}
+		} else {
+			fmt.Println("Event was null")
+		}
+
+		if len(logQueue) > 0 {
+			input := cloudwatchlogs.PutLogEventsInput{
+				LogEvents:    logQueue,
+				LogGroupName: &logGroupName,
+			}
+
+			if sequenceToken == "" {
+				err := createLogStream()
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				input = *input.SetSequenceToken(sequenceToken)
+			}
+
+			input = *input.SetLogStreamName(logStreamName)
+
+			resp, err := cwl.PutLogEvents(&input)
+			if err != nil {
+				log.Println(err)
+			}
+
+			if resp != nil {
+				sequenceToken = *resp.NextSequenceToken
+			}
+
+			logQueue = []*cloudwatchlogs.InputLogEvent{}
+		}
+
+		time.Sleep(time.Second * 5)
+	}
+
+}
